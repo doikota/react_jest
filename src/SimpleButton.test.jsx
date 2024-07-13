2286f20422c0493706b0e68911ebba3ef75d2726
doikota <doikota@gmail.com>
2024-07-13 10:25:51 +0900

    Rewrite test using describe

--- src/SimpleButton.test.jsx
+++ src/SimpleButton.test.jsx
@@ -4,16 +4,19 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import userEvent from "@testing-library/user-event";
 
-test("ボタンをクリックするとON/OFFの表示が切り替わる", async () => {
-  render(<SimpleButton />);
-  const simpleButton = screen.getByRole("button");
-  expect(simpleButton).toHaveTextContent("OFF");
-  const user = userEvent.setup();
-  await user.click(simpleButton);
-  expect(simpleButton).toHaveTextContent("ON");
-});
+describe("ボタンのテスト", () => {
+  test("ボタンをクリックするとON/OFFの表示が切り替わる", async () => {
+    render(<SimpleButton />);
+    const simpleButton = screen.getByRole("button");
+    expect(simpleButton).toHaveTextContent("OFF");
+    const user = userEvent.setup();
+    await user.click(simpleButton);
+    expect(simpleButton).toHaveTextContent("ON");
+  });
 
-test("描画されてすぐはOFFと表示されている", () => {
-  const view = render(<SimpleButton />);
-  expect(view.container).toMatchSnapshot();
+  test("描画されてすぐはOFFと表示されている", () => {
+    const view = render(<SimpleButton />);
+    expect(view.container).toMatchSnapshot();
+  });
+  
 });
