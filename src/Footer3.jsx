6e7aea07c46518d5ffcda66f45ca35868b48e0eb
doikota <doikota@gmail.com>
2024-07-21 18:34:32 +0900

    Consumer経由でプロパティを受け取る

--- src/Footer3.jsx
+++ src/Footer3.jsx
@@ -2,9 +2,17 @@ import React, { useContext } from "react";
 import { UserCount } from "./App.jsx"
 
 export const Footer3 = () => {
-  // コンテキスト経由でプロパティを受け取る
-  const count = useContext(UserCount);
+  // useContext経由でプロパティを受け取る
+  const countout = useContext(UserCount);
   return (
-    <h3>Footer3A {count}</h3>
+    <>
+      <h3>Footer3A {countout}</h3>
+      <UserCount.Consumer>
+        {/* Consumer経由でプロパティを受け取る */}
+        {(countin) => {
+          return <h3>Footer3B {countin}</h3>;
+        }}
+      </UserCount.Consumer>
+    </>
   );
-}
+};
